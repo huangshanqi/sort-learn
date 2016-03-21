e7e87be3b00734d23fa21c052be7efd76d80d17a
huangshanqi <hsqmobile@gmail.com>
2016-03-21 23:41:01 +0800

    快速排序以及归并排序

--- src/cn/evilcoder/BubbleSort.java
+++ src/cn/evilcoder/BubbleSort.java
@@ -14,7 +14,7 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] nums = RandomArray.genArrays(limit,len);
         System.out.println("before:"+Arrays.toString(nums));
-        shellSort(nums);
+        mergeSort(nums, 0, nums.length - 1);
         System.out.println("after　:"+Arrays.toString(nums));
     }
     public static void bubbleSort(int[] nums){
@@ -73,5 +73,63 @@ public class BubbleSort {
         }
     }
 
+    public static int partition(int[] nums,int start,int end,int pivotIndex){
+        //这个循环比一般的写法简洁高效,就是先统计比基准值小的个数smaller,最后基准值就放在smaller上了
+        int pivot = nums[pivotIndex];
+        int smaller = start;
+        Utils.swap(nums,pivotIndex,end);
+        for(int i=start;i<end;i++){
+            if(nums[i]<pivot){
+                Utils.swap(nums,i,smaller);
+                smaller++;
+            }
+        }
+        Utils.swap(nums,smaller,end);
+        return smaller;
+    }
+
+    public static void quickSort(int[] nums,int start,int end){
+        if(start<end){
+            int pivot = partition(nums,start,end,start);
+            System.out.println(pivot+":"+Arrays.toString(nums));
+            quickSort(nums,start,pivot-1);
+            quickSort(nums,pivot+1,end);
+        }
+    }
+
+    public static void mergeSort(int[] nums,int start,int end){
+        if(start<end){
+            int mid = start + (end - start)/2;
+            mergeSort(nums,start,mid);
+            mergeSort(nums,mid+1,end);
+            merge(nums,start,mid,end);
+        }
+    }
+
+    public static void merge(int[] nums,int start,int mid,int end){
+        //临时数组存储中间需要合并的的元素
+        int[] copy = new int[end+1-start];
+        //已排序数组索引
+        int count = 0;
+        int left  = start;
+        int right = mid+1;
+        while(left<=mid && right<=end){
+            if(nums[left]<nums[right]){
+                copy[count++] = nums[left++];
+            }else{
+                copy[count++] = nums[right++];
+            }
+        }
+        while (left<=mid){
+            copy[count++] = nums[left++];
+        }
+        while (right<=end){
+            copy[count++] = nums[right++];
+        }
+        //将临时已排序的数组复制回原数组的[start,end]
+        for(count=0;count<copy.length;count++){
+            nums[start++] = copy[count];
+        }
+    }
 
 }
