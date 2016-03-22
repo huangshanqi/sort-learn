ec416d6378973b211f042f34c9b27d32b2d4f25c
huangshanqi <huangshanqi@neotel.co,.cn>
2016-03-22 11:54:05 +0800

    堆排序

--- src/cn/evilcoder/BubbleSort.java
+++ src/cn/evilcoder/BubbleSort.java
@@ -11,12 +11,12 @@ import java.util.List;
  */
 public class BubbleSort {
 
-    private static final int limit = 1000000;
-    private static final int len = 500;
+    private static final int limit = 100;
+    private static final int len = 20;
     public static void main(String[] args) {
         int[] nums = RandomArray.genArrays(limit,len);
         System.out.println("before:"+Arrays.toString(nums));
-        radixSort(nums);
+        heapSort(nums);
         System.out.println("after　:"+Arrays.toString(nums));
     }
     public static void bubbleSort(int[] nums){
@@ -170,7 +170,34 @@ public class BubbleSort {
                 list.clear();
             }
         }
+    }
 
+
+    public static void heapSort(int[] nums){
+        for(int i=nums.length/2-1;i>=0;i--){
+            maxHeapAjust(nums,i,nums.length-1);
+        }
+
+        for(int i=nums.length-1;i>0;i--){
+            Utils.swap(nums,0,i);
+            maxHeapAjust(nums,0,i-1);
+        }
+    }
+
+    public static void maxHeapAjust(int[] nums,int start,int end){
+        int left = start*2;
+        int right = start*2+1;
+        int max = start;
+        if(left<=end && nums[left]>nums[max]){
+            max = left;
+        }
+        if(right<=end && nums[right]>nums[max]){
+            max = right;
+        }
+        if(max!=start){
+            Utils.swap(nums,max,start);
+            maxHeapAjust(nums,max,end);
+        }
     }
 
 }
